c8c314cbc5c67a675af923e8b49c80c8e7875fce
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-09 18:45:49 -0300

    style: responsive layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,9 @@ import { ModeProvider } from '@/providers/ModeProvider'
 export default function Home() {
   return (
     <main className="flex w-full justify-center">
-      <div className="flex flex-col w-full max-w-6xl p-10">
+      <div className="flex flex-col w-full max-w-6xl px-4 py-10 md:p-10">
         <Header />
-        <div className='grid grid-cols-2 pt-12 gap-9'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 pt-12 gap-9'>
           <ModeProvider>
             <DataSession />
             <ToDoList />
--- src/components/DataSession/CurrentMode.tsx
+++ src/components/DataSession/CurrentMode.tsx
@@ -5,7 +5,7 @@ export default function CurrentMode() {
     <div className='flex justify-between items-center'>
       <div>
         <p className="text-zinc-500 text-lg font-semibold">Modo atual:</p>
-        <p className="text-zinc-400 font-medium">Ciclo atual do cronômetro</p>
+        <p className="text-zinc-400 font-medium hidden md:visible">Ciclo atual do cronômetro</p>
       </div>
       <div>
         <Modes selectMode='currentMode' />
