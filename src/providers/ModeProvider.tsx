d93f3e955f314a896670128fc962aa807d468de4
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-08 21:16:03 -0300

    feat: create ModeProvider

---
+++ src/providers/ModeProvider.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { ReactNode, createContext, useContext, useState } from 'react'
+
+export interface ModeProps {
+  mode: 'focus' | 'shortPause' | 'longPause';
+}
+
+export interface ModeContextProps {
+  mode: ModeProps
+  changeMode: (newMode: ModeProps) => void
+}
+
+export const ModeContext = createContext({} as ModeContextProps)
+
+export function useMode() {
+  const context = useContext(ModeContext)
+  if (!context) {
+    throw new Error('useMode must be used within a ModeProvider')
+  }
+  return context
+}
+
+export function ModeProvider({ children }: { children: ReactNode }) {
+  const [mode, setMode] = useState<ModeProps>({ mode: 'focus' })
+
+  function changeMode(newMode: ModeProps) {
+    setMode(newMode)
+  }
+
+  return (
+    <ModeContext.Provider value={{ mode, changeMode }}>
+      {children}
+    </ModeContext.Provider>
+  )
+}
