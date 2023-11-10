b01c658904c0a1d94071dec29b1b08df268a8201
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-10 06:53:30 -0300

    feat: remove useMode funcion

--- src/providers/ModeProvider.tsx
+++ src/providers/ModeProvider.tsx
@@ -1,20 +1,12 @@
 'use client'
 import TaskService from '@/services/TaskServices'
-import { ReactNode, createContext, useContext } from 'react'
+import { ReactNode, createContext } from 'react'
 
 export interface ModeContextProps {
   data: TaskService
 }
 
 export const ModeContext = createContext({} as ModeContextProps)
-
-export function useMode() {
-  const context = useContext(ModeContext)
-  if (!context) {
-    throw new Error('useMode must be used within a ModeProvider')
-  }
-  return context
-}
 
 export function ModeProvider({ children }: { children: ReactNode }) {
   const data = new TaskService()
