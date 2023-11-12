0d71c3761afb60d36a30179a440f66ee64fa8fcb
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-12 11:35:04 -0300

    fix: correction in toggle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,9 @@
-'use client'
-import { Github, Languages, Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
-import { Button } from '../ui/button'
+import { Github, Languages } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/app/(home)/ThemeToggle'
 
 export default function Header() {
-  const { setTheme, theme } = useTheme()
-
-  function handleSetTheme() {
-    theme === 'dark' ? setTheme('light') : setTheme('dark')
-  }
-  
+ 
   return (
     <header className='flex w-full justify-between items-center'>
       <div>
@@ -17,12 +11,7 @@ export default function Header() {
         <p className='text-foreground/50'>Gerencie seu tempo de maneira mágica!</p>
       </div>
       <div className='flex gap-2'>
-        <Button
-          variant={'ghost'}
-          onClick={handleSetTheme}
-          className='flex justify-center items-center h-10 w-10 p-2 rounded-lg bg-background text-foreground/70'>
-          {theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
-        </Button>
+        <ThemeToggle />
 
         <Button variant={'ghost'} className='flex justify-center items-center h-10 w-10 p-2 rounded-lg bg-background text-foreground/70'>
           <Github size={18} />
